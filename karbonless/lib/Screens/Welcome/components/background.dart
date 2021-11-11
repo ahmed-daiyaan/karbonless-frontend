ae8f78fd57a85cebefa2e791f34d3d53524e10f9
harshitha <harshithadevineni16@gmail.com>
2021-11-11 07:38:48 +0530

    first three screens added

---
+++ karbonless/lib/Screens/Welcome/components/background.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/main_top.png",
+              width: size.width * 0.3,
+              color: const Color.fromRGBO(255, 255, 255, 0.5),
+              colorBlendMode: BlendMode.modulate,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/main_bottom.png",
+              width: size.width * 0.2,
+              color: const Color.fromRGBO(255, 255, 255, 0.5),
+              colorBlendMode: BlendMode.modulate,
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
